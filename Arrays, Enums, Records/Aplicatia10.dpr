8e69d834aa227a466f2f7f34635a024acde886ad
gdinu <g.dinu@attosoft.ro>
2014-01-14 00:00:17 +0200

    no message

--- Arrays, Enums, Records/Aplicatia10.dpr
+++ Arrays, Enums, Records/Aplicatia10.dpr
@@ -6,9 +6,10 @@ uses
   SysUtils, Math;
 
 var
-  vNumere, i: integer;
+  vNumere, i, vMin, vMax: integer;
   vArray: array of integer;
 begin
+
   write('Introduceti cate numere vreti sa aiba multimea de numere introduse: ');
   readln(vNumere);
 
@@ -19,8 +20,19 @@ begin
     write('Introduceti numar: ');
     readln(vArray[i]);
   end;
-  writeln('Valoarea minima este: ' + IntToStr(MinIntValue(vArray)) +
-  #13#10 + 'Valoarea maxima este: ' + IntToStr(MaxIntValue(vArray)));
+
+  vMin := vArray[0];
+  vMax := vArray[0];
+  for i := 0 to length(vArray) - 1 do
+  begin
+    if vMin > vArray[i] then
+      vMin := vArray[i];
+    if vMax < vArray[i] then
+      vMax := vArray[i];
+  end;
+
+  writeln('Valoarea minima este: ' + IntToStr(vMin) +
+    #13#10 + 'Valoarea maxima este: ' + IntToStr(vMax));
   readln;
 end.
 
