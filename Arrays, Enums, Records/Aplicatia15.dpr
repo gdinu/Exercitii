12e0099618378b53246c27d9c4898d556e86e016
gdinu <g.dinu@attosoft.ro>
2014-01-15 23:45:00 +0200

    no message

--- Arrays, Enums, Records/Aplicatia15.dpr
+++ Arrays, Enums, Records/Aplicatia15.dpr
@@ -7,16 +7,19 @@ uses
 type
   TZile = (Luni = 1, Marti, Miercuri, Joi, Vineri, Sambata, Duminica);
 var
-  i, vnr: integer;
-  vNumarZileLucratoare: TZile;
+  vnr: integer;
   vZiLucratoare: set of 1..5;
 const
   cZile: array[1..7] of string = ('Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata', 'Duminica');
 
 begin
+  vZiLucratoare := [ord(Luni)..ord(Vineri)];
+  repeat
   Write('Introduceti numarul zilei din saptamana: ');
   readln(vnr);
+  until vnr in [1..7];
   write(cZile[vnr]);
+
 
   if vnr in vZiLucratoare then
     writeln(' este zi lucratoare!')
